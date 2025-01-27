936c6a644f5135fd53b08c2b48930bc4e2e62d19
Luan Silva <luan.ss5500@gmail.com>
2025-01-27 16:52:31 -0300

    JS Code

---
+++ script.js
@@ -0,0 +1,20 @@
+const menuIcon = document.querySelector("#menu-icon");
+const navLinks = document.querySelector(".nav-links");
+
+menuIcon.onclick = () => {
+  navLinks.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  navLinks.classList.remove("active");
+};
+
+/* Typing text animator */
+
+const typed = new Typed(".multiple-text", {
+  strings: ["Desenvolvedor Fronend", "Suporte Técnico"],
+  typeSpeed: 60,
+  backSpeed: 60,
+  backDelay: 100,
+  loop: true,
+});
